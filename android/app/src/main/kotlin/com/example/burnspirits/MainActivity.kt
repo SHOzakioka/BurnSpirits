42d4d86fdd3aa129329464098dbe8ed78f7b3bc5
okazaki sho <zakioka.sho@gmail.com>
2023-08-16 13:49:47 +0900

    first Commit.

---
+++ android/app/src/main/kotlin/com/example/burnspirits/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.burnspirits
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 画像
+  final img = Image.asset(
+    'images/penguin.jpeg',
+  );
+
+  // ロー
+  final row = Row(
+    //mainAxisAlignment: MainAxisAlignment.center,
+    //crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      img
+    //Text('ばななまん')
+    ],
+  );
+
+  // アプリ
+  final a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: row,
+      ),
+    ),
+  );
+  runApp(a);
+}

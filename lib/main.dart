5a1235ea06b0c0b44406da746afe10f355c427f2
okazaki sho <zakioka.sho@gmail.com>
2023-08-25 01:58:35 +0900

    bananaカウンターを表示するmain.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:burnspirits/features/wigets/banana_counter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,24 +10,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    // 画像
-    final img = Image.asset(
-      'assets/images/penguin.jpg',
+
+    //バナナカウンター
+    const bnn = BananaCounter(
+      number: 888,
     );
 
-    // ロー
-    final row = Row(
-      //mainAxisAlignment: MainAxisAlignment.center,
-      //crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        img
-        //Text('ばななまん')
-      ],
-    );
+    //マテリアルapp
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: row,
+          child: bnn,
         ),
       ),
     );
